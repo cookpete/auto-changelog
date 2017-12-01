14cc6392ea3d1722801c84be17997757f387ba2d
Pete Cook <pete@cookpete.com>
2017-12-01 07:45:44 +0000

    Use commander instance

--- src/run.js
+++ src/run.js
@@ -1,4 +1,4 @@
-import commander from 'commander'
+import { Command } from 'commander'
 import { readJson, writeFile, pathExists } from 'fs-extra'
 
 import { version } from '../package.json'
@@ -16,7 +16,7 @@ const NPM_VERSION_TAG_PREFIX = 'v'
 const PACKAGE_OPTIONS_KEY = 'auto-changelog'
 
 function getOptions (argv, pkg) {
-  const options = commander
+  const options = new Command()
     .option('-o, --output [file]', `output file, default: ${DEFAULT_OUTPUT}`, DEFAULT_OUTPUT)
     .option('-t, --template [template]', `specify template to use [compact, keepachangelog, json], default: ${DEFAULT_TEMPLATE}`, DEFAULT_TEMPLATE)
     .option('-r, --remote [remote]', `specify git remote to use for links, default: ${DEFAULT_REMOTE}`, DEFAULT_REMOTE)
