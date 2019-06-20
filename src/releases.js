f05dfe06319b4319de0416a7121fb9b16907c4bd
Pete Cook <pete@cookpete.com>
2019-06-20 13:20:31 +0100

    Hyphenate date-desc and add to readme/help

--- src/releases.js
+++ src/releases.js
@@ -153,7 +153,7 @@ function commitSorter ({ sortCommits }) {
     if (!a.breaking && b.breaking) return 1
     if (a.breaking && !b.breaking) return -1
     if (sortCommits === 'date') return new Date(a.date) - new Date(b.date)
-    if (sortCommits === 'datedesc') return new Date(b.date) - new Date(a.date)
+    if (sortCommits === 'date-desc') return new Date(b.date) - new Date(a.date)
     return (b.insertions + b.deletions) - (a.insertions + a.deletions)
   }
 }
